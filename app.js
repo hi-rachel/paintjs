8c1d5e0c6f7938379ccae8b2b7303c48b99fc658
hi-rachel <rachel.uiux@gmail.com>
2022-06-18 00:12:53 +0900

    add margin

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
 
 ctx.fillStyle = "white";
-ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+ctx.fillRect(0, 0, 700, 700);
 ctx.strokeStyle = INITIAL_COLOR;
 ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
